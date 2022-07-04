4213c3223cf2134c8799c287b99d74c308822fe8
Wh1teTee <107686488+Wh1teTee@users.noreply.github.com>
2022-07-04 10:00:32 +0300

    Add files via upload

---
+++ Работа с LINQ/Задание 4-30.06.2022/Задание 4-30.06.2022/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Задание_4_30._06._2022
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            players.Add(new Player("Arabic-Dev95", 100, 100));
+            players.Add(new Player("Iterry", 45, 30));
+            players.Add(new Player("Quader", 47, 40));
+            players.Add(new Player("Elianasya", 67, 48));
+            players.Add(new Player("Bara", 90, 88));
+            players.Add(new Player("Janato", 1, 10));
+            players.Add(new Player("Hero", 5, 15));
+            players.Add(new Player("Anailaren", 22, 34));
+            players.Add(new Player("Nnislinaliss", 39, 50));
+            players.Add(new Player("Naloosephy", 80, 100));
+            foreach(Player player in players)
+            {
+                LB.Content += player.Name + " Сила: " + player.Strong + " Уровень: " + player.Lvl + "\n";
+            }
+        }
+        List<Player> players = new List<Player>();
+
+        private void BStrong_Click(object sender, RoutedEventArgs e)
+        {
+            LB.Content = "";
+            var Quary =
+                from plaer in players
+                orderby plaer.Strong descending
+                select plaer;
+            int counter = 0;
+            foreach (Player player in Quary)
+            {
+                LB.Content += player.Name + " Сила: " + player.Strong + " Уровень: " + player.Lvl + "\n";
+                counter++;
+                if (counter == 3)
+                    break;
+            }
+        }
+
+        private void B1_Click(object sender, RoutedEventArgs e)
+        {
+            LB.Content = "";
+            foreach (Player player in players)
+            {
+                LB.Content += player.Name + " Сила: " + player.Strong + " Уровень: " + player.Lvl + "\n";
+            }
+        }
+
+        private void BLvl_Click(object sender, RoutedEventArgs e)
+        {
+            LB.Content = "";
+            var Quary =
+                from plaer in players
+                orderby plaer.Lvl descending
+                select plaer;
+            int counter = 0;
+            foreach (Player player in Quary)
+            {
+                LB.Content += player.Name + " Сила: " + player.Strong + " Уровень: " + player.Lvl + "\n";
+                counter++;
+                if (counter == 3)
+                    break;
+            }
+        }
+    }
+}
---
+++ Работа с LINQ/Задание 4-30.06.2022/Задание 4-30.06.2022/Player.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_4_30._06._2022
+{
+    class Player
+    {
+        private string name;
+        private int strong;
+        private int lvl;
+        public Player(string n, int s, int l)
+        {
+            name = n;
+            strong = s;
+            lvl = l;
+        }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+        public int Strong
+        {
+            get
+            {
+                return strong;
+            }
+        }
+        public int Lvl
+        {
+            get
+            {
+                return lvl;
+            }
+        }
+    }
+}
