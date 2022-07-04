2dd840361e89fd9799bdb7ebbd4293397cfd052d
Wh1teTee <107686488+Wh1teTee@users.noreply.github.com>
2022-07-04 09:49:39 +0300

    Add files via upload

---
+++ Ввод-Вывод/Задание 2-21.06.2022/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Задание_2_21._06._2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                bool c = true;
+                StreamWriter sw = new StreamWriter("file.txt");
+                Console.WriteLine("Введите данные для записи в файл:");
+                while (c)
+                {
+                    sw.WriteLine(Console.ReadLine());
+                    Console.WriteLine("Продолжить? (y/n)");
+                    string s = Console.ReadLine();
+                    switch (s)
+                    {
+                        case "y":
+                            break;
+                        case "n":
+                            c = false;
+                            break;
+                    }
+                }
+                sw.Close();
+                Console.Clear();
+                StreamReader sr = new StreamReader("file.txt");
+                Console.WriteLine(sr.ReadToEnd());
+                sr.Close();
+                Console.ReadKey();
+            }
+            catch (Exception E)
+            {
+                Console.WriteLine(E.Message);
+            }
+        }
+    }
+}
---
+++ Ввод-Вывод/Задание 3-21.06.2022/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Compression;
+
+namespace Задание_3_21._06._2022
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string path;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                WriteAllText(path, textBox1.Text);
+                label1.Text = "Файл сохранён";
+            }
+            catch (Exception E)
+            {
+                MessageBox.Show(E.Message);
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                path = textBox2.Text + ".txt";
+                textBox1.Text = ReadAllText(path);
+            }
+            catch (Exception E)
+            {
+                MessageBox.Show(E.Message);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            FileStream ofs = File.Open(path, FileMode.Open);
+            FileStream cfs = File.Create(path + ".gz");
+            GZipStream compressor = new GZipStream(cfs, CompressionMode.Compress);
+            ofs.CopyTo(compressor);
+            compressor.Close();
+            ofs.Close();
+            cfs.Close();
+            label1.Text = "Файл успешно сжат";
+        }
+        public static string ReadAllText(string path)
+        {
+            using (FileStream stream = File.OpenRead(path))
+            {
+                var encoding = new UTF8Encoding(true);
+                StreamReader reader = new StreamReader(stream, encoding);
+                return reader.ReadToEnd();
+            }
+        }
+        public static void WriteAllText(string path, string text)
+        {
+            using (FileStream stream = File.Open(path, FileMode.Open, FileAccess.Write))
+            {
+                byte[] info = new UTF8Encoding(true).GetBytes(text);
+                stream.Write(info, 0, info.Length);
+                //var encoding = new UTF8Encoding(true);
+                //StreamWriter writer = new StreamWriter(stream, encoding);
+                //writer.WriteLine(text);
+            }
+        }
+    }
+}
+
